4efaf708835d93b771a09ef8db12addb7d53be43
Muzhda Ehsan <muzhda.ehsan@dcmail.ca>
2021-04-16 22:15:49 -0400

    changes made

--- .history/Server/Config/db_20210416220429.ts
+++ .history/Server/Config/db_20210416220429.ts
@@ -1,5 +1,5 @@
 export const LocalURI = "mongodb://localhost/webd6201";
-export const RemoteURI = "mongodb+srv://muzhda:tRGc2TvXjjWLtfr0@webd6201.mh7df.mongodb.net/WEBD6201?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://muzhda:1O9cQciPDyKVp41R@webd6201.mh7df.mongodb.net/WEBD6201?retryWrites=true&w=majority";
 //export const RemoteURI = "mongodb+srv://thomas:5wWyrxbVa59twJoh@webd6201.zvqij.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const Secret = "someSecret";
 export const Host = "MongoDB Atlas";
